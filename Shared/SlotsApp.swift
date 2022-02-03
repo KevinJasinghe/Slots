fa968ebdf516a1949c5e8b8725c2b3ee07b9c8b8
Kevin <kevinjasinghe@gmail.com>
2022-02-02 19:25:23 -0500

    Initial Commit

---
+++ Shared/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Shared
+//
+//  Created by Kevin Jasinghe on 2022-02-02.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
